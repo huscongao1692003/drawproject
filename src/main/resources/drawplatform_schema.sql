63727a9f7fd92485707bb16d1d98cd270ed6489d
Le Huy <huy110903@gmail.com>
2023-09-20 11:11:14 +0700

    update script sql

--- src/main/resources/drawplatform_schema.sql
+++ src/main/resources/drawplatform_schema.sql
@@ -1,238 +1,192 @@
 use drawcourses;
 
 CREATE TABLE `skills` (
-                          `skill_id` integer primary key auto_increment not null,
-                          `skill_name` varchar(255) not null
-);
-
-
-
-CREATE TABLE `users` (
-                         `user_id` integer PRIMARY KEY auto_increment,
-                         `username` varchar(255) not null,
-                         `avatar` longblob null,
-                         `pwd` varchar(255) not null,
-                         `email` varchar(255) not null,
-                         `mobile_num` varchar(255),
-                         `status` varchar(255) not null,
-                         `skill_id` integer not null,
-                         `role_id` integer not null,
-                         `created_at` timestamp,
-                         `updated_at` timestamp,
-                         FOREIGN KEY (`skill_id`) REFERENCES `skills`(`skill_id`)
-);
-
-CREATE TABLE `posts` (
-                         `post_id` integer PRIMARY KEY auto_increment,
-                         `title` varchar(255) not null,
-                         `category_id` integer not null,
-                         `description` varchar(255) not null,
-                         `reading_time` integer not null,
-                         `image` longblob,
-                         `body` text not null,
-                         `user_id` integer not null,
-                         `status` varchar(255) not null,
-                         `created_at` timestamp,
-                         `updated_at` timestamp
-);
-
-CREATE TABLE `courses` (
-                           `course_id` integer PRIMARY KEY auto_increment,
-                           `course_title` varchar(255) not null,
-                           `description` varchar(255) not null,
-                           `information` text not null,
-                           `skill_id` integer not null,
-                           `price` integer not null,
-                           `category_id` integer not null,
-                           `image` longblob not null,
-                           `status` varchar(255) not null,
-                           `created_at` timestamp,
-                           `updated_at` timestamp,
-                           FOREIGN KEY (`skill_id`) REFERENCES `skills`(`skill_id`)
-);
-
-CREATE TABLE `rolling_style` (
-                                 `rolling_style_id` integer auto_increment primary key,
-                                 `rolling_style_name` varchar(255) not null
-);
-
-CREATE TABLE `category` (
-                            `category_id` integer auto_increment primary key,
-                            `category_name` varchar(100) not null
-);
-
-CREATE TABLE `user_courses` (
-                                `user_id` integer not null,
-                                `course_id` integer not null
-);
-
-CREATE TABLE `contact_msg` (
-                               `contact_id` integer PRIMARY KEY auto_increment,
-                               `name` varchar(255) not null,
-                               `mobile_num` varchar(255) not null,
-                               `email` varchar(255) not null,
-                               `message` varchar(255) not null,
-                               `status` varchar(255) not null,
-                               `created_at` timestamp,
-                               `updated_at` timestamp
-);
-
-CREATE TABLE `subscribe` (
-                             `subscribe_id` integer PRIMARY KEY auto_increment,
-                             `email` varchar(255) not null
+    `skill_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `skill_name` VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE `roles` (
-                         `role_id` integer PRIMARY KEY auto_increment,
-                         `role_name` varchar(255) not null
+    `role_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `role_name` VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE `category` (
+    `category_id` INT AUTO_INCREMENT PRIMARY KEY,
+    `category_name` VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE `rolling_style` (
+    `rolling_style_id` INT AUTO_INCREMENT PRIMARY KEY,
+    `rolling_style_name` VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE `users` (
+    `user_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `username` VARCHAR(255) NOT NULL,
+    `avatar` LONGBLOB,
+    `pwd` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(255) NOT NULL,
+    `mobile_num` VARCHAR(255),
+    `status` VARCHAR(255) NOT NULL,
+    `skill_id` INT NOT NULL,
+    `role_id` INT NOT NULL,
+    `created_at` TIMESTAMP,
+    `updated_at` TIMESTAMP,
+    FOREIGN KEY (`skill_id`) REFERENCES `skills`(`skill_id`),
+    FOREIGN KEY (`role_id`) REFERENCES `roles`(`role_id`)
+);
+
+CREATE TABLE `posts` (
+    `post_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `title` VARCHAR(255) NOT NULL,
+    `category_id` INT NOT NULL,
+    `description` VARCHAR(255) NOT NULL,
+    `reading_time` INT NOT NULL,
+    `image` LONGBLOB,
+    `body` TEXT NOT NULL,
+    `user_id` INT NOT NULL,
+    `status` VARCHAR(255) NOT NULL,
+    `created_at` TIMESTAMP,
+    `updated_at` TIMESTAMP,
+    FOREIGN KEY (`user_id`) REFERENCES `users`(`user_id`),
+    FOREIGN KEY (`category_id`) REFERENCES `category`(`category_id`)
+);
+
+CREATE TABLE `courses` (
+    `course_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `course_title` VARCHAR(255) NOT NULL,
+    `description` VARCHAR(255) NOT NULL,
+    `information` TEXT NOT NULL,
+    `skill_id` INT NOT NULL,
+    `price` INT NOT NULL,
+    `category_id` INT NOT NULL,
+    `image` LONGBLOB NOT NULL,
+    `status` VARCHAR(255) NOT NULL,
+    `rolling_style_id` INT NOT NULL,
+    `created_at` TIMESTAMP,
+    `updated_at` TIMESTAMP,
+    FOREIGN KEY (`skill_id`) REFERENCES `skills`(`skill_id`),
+    FOREIGN KEY (`category_id`) REFERENCES `category`(`category_id`),
+    FOREIGN KEY (`rolling_style_id`) REFERENCES `rolling_style`(`rolling_style_id`)
+);
+
+CREATE TABLE `user_courses` (
+    `user_id` INT NOT NULL,
+    `course_id` INT NOT NULL,
+    PRIMARY KEY (`user_id`, `course_id`),
+    FOREIGN KEY (`user_id`) REFERENCES `users`(`user_id`),
+    FOREIGN KEY (`course_id`) REFERENCES `courses`(`course_id`)
+);
+
+CREATE TABLE `topic` (
+    `topic_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `topic_title` VARCHAR(255),
+    `course_id` INT,
+    FOREIGN KEY (`course_id`) REFERENCES `courses`(`course_id`)
+);
+
+CREATE TABLE `lesson` (
+    `lesson_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `url` VARCHAR(255),
+    `topic_id` INT,
+    `name` VARCHAR(255),
+    `type_file` VARCHAR(255),
+    `created_at` TIMESTAMP,
+    `updated_at` TIMESTAMP,
+    FOREIGN KEY (`topic_id`) REFERENCES `topic`(`topic_id`)
+);
+
+
+CREATE TABLE `contact_msg` (
+    `contact_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `mobile_num` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(255) NOT NULL,
+    `message` VARCHAR(255) NOT NULL,
+    `status` VARCHAR(255) NOT NULL,
+    `created_at` TIMESTAMP,
+    `updated_at` TIMESTAMP
+);
+
+CREATE TABLE `subscribe` (
+    `subscribe_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `email` VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE `orders` (
-                          `order_id` integer PRIMARY KEY auto_increment,
-                          `price` integer not null,
-                          `corrency` varchar(255) not null,
-                          `method` varchar(255) not null,
-                          `description` varchar(255) null,
-                          `user_id` integer not null,
-                          `order_date` timestamp
+    `order_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `price` INT NOT NULL,
+    `method` VARCHAR(255) NOT NULL,
+    `description` VARCHAR(255),
+    `user_id` INT NOT NULL,
+    `order_date` TIMESTAMP,
+    FOREIGN KEY (`user_id`) REFERENCES `users`(`user_id`)
 );
 
 CREATE TABLE `order_details` (
-                                 `course_id` integer not null,
-                                 `orders_id` integer not null,
-                                 PRIMARY KEY (`course_id`, `orders_id`)
+    `course_id` INT NOT NULL,
+    `orders_id` INT NOT NULL,
+    PRIMARY KEY (`course_id`, `orders_id`),
+    FOREIGN KEY (`course_id`) REFERENCES `courses`(`course_id`),
+    FOREIGN KEY (`orders_id`) REFERENCES `orders`(`order_id`)
 );
 
 CREATE TABLE `feedback` (
-                            `feedback_id` integer PRIMARY KEY auto_increment,
-                            `feedback_information` varchar(255) not null,
-                            `status` varchar(255) not null,
-                            `star` integer not null,
-                            `created_at` timestamp,
-                            `updated_at` timestamp,
-                            `course_id` integer not null,
-                            `user_id` integer not null
-);
-
-CREATE TABLE `video_url` (
-                             `video_id` integer PRIMARY KEY auto_increment,
-                             `video_url` varchar(255) not null,
-                             `course_id` integer not null,
-                             `created_at` timestamp,
-                             `updated_at` timestamp
+    `feedback_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `feedback_information` VARCHAR(255) NOT NULL,
+    `status` VARCHAR(255) NOT NULL,
+    `star` INT NOT NULL,
+    `created_at` TIMESTAMP,
+    `updated_at` TIMESTAMP,
+    `course_id` INT NOT NULL,
+    `user_id` INT NOT NULL,
+    FOREIGN KEY (`course_id`) REFERENCES `courses`(`course_id`),
+    FOREIGN KEY (`user_id`) REFERENCES `users`(`user_id`)
 );
 
 CREATE TABLE `comment` (
-                           `comment_id` integer PRIMARY KEY auto_increment,
-                           `user_id` integer not null,
-                           `comment_value` varchar(255) not null,
-                           `post_id` integer not null,
-                           `course_id` integer not null,
-                           `created_at` timestamp,
-                           `status` varchar(255) not null
+    `comment_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `user_id` INT NOT NULL,
+    `comment_value` VARCHAR(255) NOT NULL,
+    `post_id` INT NULL,
+    `course_id` INT NULL,
+    `created_at` TIMESTAMP,
+    `status` VARCHAR(255) NOT NULL,
+    FOREIGN KEY (`user_id`) REFERENCES `users`(`user_id`),
+    FOREIGN KEY (`post_id`) REFERENCES `posts`(`post_id`),
+    FOREIGN KEY (`course_id`) REFERENCES `courses`(`course_id`)
 );
 
 CREATE TABLE `homework` (
-                            `homework_id` integer PRIMARY KEY auto_increment,
-                            `homework_title` varchar(255) not null,
-                            `topic` text not null,
-                            `created_at` timestamp,
-                            `updated_at` timestamp,
-                            `course_id` integer not null,
-                            `status` varchar(255) not null,
-                            FOREIGN KEY (`course_id`) REFERENCES `courses` (`course_id`)
+    `homework_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `homework_title` VARCHAR(255) NOT NULL,
+    `topic` TEXT NOT NULL,
+    `created_at` TIMESTAMP,
+    `updated_at` TIMESTAMP,
+    `course_id` INT NOT NULL,
+    `status` VARCHAR(255) NOT NULL,
+    FOREIGN KEY (`course_id`) REFERENCES `courses`(`course_id`)
 );
 
 CREATE TABLE `collection` (
-                              `collection_id` integer PRIMARY KEY  auto_increment,
-                              `user_id` integer not null,
-                              `artwork` longblob not null,
-                              `cetificate` varchar(255) not null,
-                              `experiment` varchar(255) not null
+    `collection_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `user_id` INT NOT NULL,
+    `artwork` LONGBLOB NOT NULL,
+    `bio` VARCHAR(255) NULL,
+    `certificate` VARCHAR(255) NOT NULL,
+    `experiment` VARCHAR(255) NOT NULL,
+    FOREIGN KEY (`user_id`) REFERENCES `users`(`user_id`)
 );
 
 CREATE TABLE `user_homework` (
-                                 `task_id` integer primary key auto_increment,
-                                 `user_id` integer not null,
-                                 `task_title` varchar(255) not null,
-                                 `description` varchar(255) ,
-                                 `image` longblob not null,
-                                 `created_at` timestamp,
-                                 `updated_at` timestamp,
-                                 `homework_id` integer not null
+    `task_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `user_id` INT NOT NULL,
+    `task_title` VARCHAR(255) NOT NULL,
+    `description` VARCHAR(255),
+    `image` LONGBLOB NOT NULL,
+    `created_at` TIMESTAMP,
+    `updated_at` TIMESTAMP,
+    `homework_id` INT NOT NULL,
+    FOREIGN KEY (`user_id`) REFERENCES `users`(`user_id`),
+    FOREIGN KEY (`homework_id`) REFERENCES `homework`(`homework_id`)
 );
-
-
-ALTER TABLE `courses` ADD FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`);
-
-ALTER TABLE `user_courses` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
-
-ALTER TABLE `user_courses` ADD FOREIGN KEY (`course_id`) REFERENCES `courses` (`course_id`);
-
-ALTER TABLE `comment` ADD FOREIGN KEY (`post_id`) REFERENCES `posts` (`post_id`);
-
-ALTER TABLE `comment` ADD FOREIGN KEY (`course_id`) REFERENCES `courses` (`course_id`);
-
-ALTER TABLE `video_url` ADD FOREIGN KEY (`course_id`) REFERENCES `courses` (`course_id`);
-
-ALTER TABLE `comment` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
-
-ALTER TABLE `collection` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
-
-
-ALTER TABLE `user_homework` ADD FOREIGN KEY (`homework_id`) REFERENCES `homework` (`homework_id`);
-
-ALTER TABLE `user_homework` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
-
-ALTER TABLE `feedback` ADD FOREIGN KEY (`course_id`) REFERENCES `courses` (`course_id`);
-
-ALTER TABLE `feedback` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
-
-ALTER TABLE `order_details` ADD FOREIGN KEY (`course_id`) REFERENCES `courses` (`course_id`);
-
-ALTER TABLE `order_details` ADD FOREIGN KEY (`orders_id`) REFERENCES `orders` (`order_id`);
-
-ALTER TABLE `orders` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
-
-ALTER TABLE `posts` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
-
-ALTER TABLE `users` ADD FOREIGN KEY (`role_id`) REFERENCES `roles` (`role_id`);
-ALTER TABLE `skills` ADD FOREIGN KEY (`skill_id`) REFERENCES `users` (`skill_id`);
-
-ALTER TABLE `skills` ADD FOREIGN KEY (`skill_id`) REFERENCES `courses` (`skill_id`);
-
-ALTER TABLE `courses` ADD FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`);
-
-ALTER TABLE `user_courses` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
-
-ALTER TABLE `user_courses` ADD FOREIGN KEY (`course_id`) REFERENCES `courses` (`course_id`);
-
-ALTER TABLE `comment` ADD FOREIGN KEY (`post_id`) REFERENCES `posts` (`post_id`);
-
-ALTER TABLE `comment` ADD FOREIGN KEY (`course_id`) REFERENCES `courses` (`course_id`);
-
-ALTER TABLE `video_url` ADD FOREIGN KEY (`course_id`) REFERENCES `courses` (`course_id`);
-
-ALTER TABLE `comment` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
-
-ALTER TABLE `collection` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
-
-ALTER TABLE `courses` ADD FOREIGN KEY (`course_id`) REFERENCES `homework` (`course_id`);
-
-ALTER TABLE `user_homework` ADD FOREIGN KEY (`homework_id`) REFERENCES `homework` (`homework_id`);
-
-ALTER TABLE `user_homework` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
-
-ALTER TABLE `feedback` ADD FOREIGN KEY (`course_id`) REFERENCES `courses` (`course_id`);
-
-ALTER TABLE `feedback` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
-
-ALTER TABLE `order_details` ADD FOREIGN KEY (`course_id`) REFERENCES `courses` (`course_id`);
-
-ALTER TABLE `order_details` ADD FOREIGN KEY (`orders_id`) REFERENCES `orders` (`order_id`);
-
-ALTER TABLE `orders` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
-
-ALTER TABLE `posts` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
-
-ALTER TABLE `users` ADD FOREIGN KEY (`role_id`) REFERENCES `roles` (`role_id`);
