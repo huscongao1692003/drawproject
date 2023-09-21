41f06de53805d710bd70df474e375bfc10f494bd
Le Huy <huy110903@gmail.com>
2023-09-21 12:01:09 +0700

    add feature get courses for each page

--- src/main/java/com/drawproject/dev/controller/CourseController.java
+++ src/main/java/com/drawproject/dev/controller/CourseController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -21,11 +22,22 @@ public class CourseController {
     @Autowired
     CategoryService categoryService;
 
+    /**
+     * Gets top course.
+     *
+     * @return the top list course by list category
+     */
     @GetMapping("/")
     public ResponseEntity<List<List<CoursePreviewDTO>>> getTopCourse() {
         return ResponseEntity.ok().body(courseService.getTopCourseByCategory());
     }
 
 
+    @GetMapping("/courses")
+    public ResponseEntity<ResponseDTO> getCourses(@RequestParam("page") int page,
+                                                            @RequestParam("eachPage") int eachPage) {
+
+        return ResponseEntity.ok().body(courseService.getCourseByCategory(page, eachPage));
+    }
 
 }
