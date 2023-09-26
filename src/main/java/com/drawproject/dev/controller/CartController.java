c31c61dfff1a04723bb5df85bc841b2d2c96cf3a
Thanh Nguyen <89590291+thanhnguyen1609@users.noreply.github.com>
2023-09-26 22:28:28 +0700

    add PaypalConfig
    Signed-off-by: Thanh Nguyen <89590291+thanhnguyen1609@users.noreply.github.com>

---
+++ src/main/java/com/drawproject/dev/controller/CartController.java
@@ -0,0 +1,16 @@
+package com.drawproject.dev.controller;
+
+import com.drawproject.dev.repository.CourseRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/cart")
+public class CartController {
+
+    @Autowired
+    CourseRepository courseRepository;
+}
