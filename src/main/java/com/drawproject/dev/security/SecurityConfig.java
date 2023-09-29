8683b4d68e313a934e6e4e3c8acc9493ab33f78c
Le Huy <huy110903@gmail.com>
2023-09-29 08:52:12 +0700

    fix endpoint

--- src/main/java/com/drawproject/dev/security/SecurityConfig.java
+++ src/main/java/com/drawproject/dev/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.drawproject.dev.constrains.DrawProjectConstaints;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -45,10 +46,10 @@ public class SecurityConfig {
                  ).permitAll()
                 .requestMatchers("/api/auth/**").permitAll()
                  .requestMatchers("/api/top-courses/**").permitAll()
-                 .requestMatchers("/api/courses/search").permitAll()
-                 .requestMatchers("/api/courses/create").authenticated()
-                 .requestMatchers("/api/courses/update").authenticated()
-                 .requestMatchers("/api/courses/{id}/feedback").permitAll()
+                 .requestMatchers(HttpMethod.GET, "/api/courses").permitAll()
+                 .requestMatchers(HttpMethod.POST, "/api/courses").authenticated()
+                 .requestMatchers(HttpMethod.PUT, "/api/courses").authenticated()
+                 .requestMatchers("/api/courses/{ipd}/feedback").permitAll()
                  .requestMatchers("/api/courses/{id}").hasRole(DrawProjectConstaints.INSTRUCTOR)
                  .requestMatchers("/api/courses/{id}/student").hasAnyRole(DrawProjectConstaints.INSTRUCTOR,
                          DrawProjectConstaints.ADMIN_ROLE, DrawProjectConstaints.STAFF)
