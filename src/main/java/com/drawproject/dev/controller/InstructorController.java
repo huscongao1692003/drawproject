92587e34528a4d48085fb862481a2289baf37f3e
Thanh Nguyen <89590291+thanhnguyen1609@users.noreply.github.com>
2023-10-13 12:24:42 +0700

    add get order for user
    Signed-off-by: Thanh Nguyen <89590291+thanhnguyen1609@users.noreply.github.com>

--- src/main/java/com/drawproject/dev/controller/InstructorController.java
+++ src/main/java/com/drawproject/dev/controller/InstructorController.java
@@ -2,24 +2,18 @@ package com.drawproject.dev.controller;
 
 import com.drawproject.dev.dto.InstructorDTO;
 import com.drawproject.dev.dto.InstructorDetailDTO;
-import com.drawproject.dev.dto.OrderAdminDTO;
-import com.drawproject.dev.model.Instructor;
-import com.drawproject.dev.model.Orders;
+import com.drawproject.dev.dto.ResponseDTO;
 import com.drawproject.dev.model.User;
-import com.drawproject.dev.repository.InstructorRepository;
-import com.drawproject.dev.repository.UserRepository;
+import com.drawproject.dev.service.InstructorService;
 import com.drawproject.dev.service.OrderService;
 import com.drawproject.dev.service.ProfileService;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 
@@ -27,20 +21,20 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/api/v1/instructor")
 public class InstructorController {
-    @Autowired
-    private UserRepository userRepository;
 
     @Autowired
     ProfileService profileService;
 
     @Autowired
+
+    ModelMapper modelMapper;
+
+    @Autowired
     OrderService orderService;
 
-   @Autowired
-   ModelMapper modelMapper;
 
-   @Autowired
-    InstructorRepository instructorRepository;
+    @Autowired
+    InstructorService instructorService;
 
     @GetMapping
     public ResponseEntity<List<InstructorDTO>> showInstructor() {
@@ -62,17 +56,20 @@ public class InstructorController {
 
     @GetMapping("/{userId}")
     public ResponseEntity<InstructorDetailDTO> showInstructorDetail(@PathVariable int userId) {
-        User instructor = userRepository.findUserByUserId(userId);
+        User instructor = profileService.findInstructorById(userId);
 
         if (instructor != null) {
             InstructorDetailDTO instructorDetailDTO = modelMapper.map(instructor, InstructorDetailDTO.class);
-            instructorDetailDTO.setInstructorId(userId);
             return ResponseEntity.ok(instructorDetailDTO);
         } else {
             return ResponseEntity.notFound().build();
         }
     }
 
+    @GetMapping("/{userId}/certificates")
+    public ResponseEntity<ResponseDTO> getCertificates(@PathVariable("userId") int userId) {
+        return ResponseEntity.ok().body(instructorService.getCertificates(userId));
+    }
 
 
 
