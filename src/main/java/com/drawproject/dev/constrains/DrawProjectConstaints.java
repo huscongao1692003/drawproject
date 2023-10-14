a2a9abfeb1e8db122c154af842cbf72423f010e3
Thanh Nguyen <89590291+thanhnguyen1609@users.noreply.github.com>
2023-10-14 18:49:37 +0700

    change role not user, its customer
    Signed-off-by: Thanh Nguyen <89590291+thanhnguyen1609@users.noreply.github.com>

--- src/main/java/com/drawproject/dev/constrains/DrawProjectConstaints.java
+++ src/main/java/com/drawproject/dev/constrains/DrawProjectConstaints.java
@@ -3,7 +3,7 @@ package com.drawproject.dev.constrains;
 public class DrawProjectConstaints {
     public static final String OPEN = "Open";
     public static final String CLOSE = "Close";
-    public static final String USER_ROLE = "USER";
+    public static final String USER_ROLE = "CUSTOMER";
     public static final String INSTRUCTOR = "INSTRUCTOR";
     public static final String STAFF = "STAFF";
     public static final String ADMIN_ROLE = "ADMIN";
