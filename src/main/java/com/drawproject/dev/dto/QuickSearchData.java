e196ea2adfb7bfc860a934fec47f4e74bd49fac7
MaxH2k3 <huy110903@gmail.com>
2023-10-29 18:44:31 +0700

    update quick search

--- src/main/java/com/drawproject/dev/dto/QuickSearchData.java
+++ src/main/java/com/drawproject/dev/dto/QuickSearchData.java
@@ -3,6 +3,7 @@ package com.drawproject.dev.dto;
 import com.drawproject.dev.dto.course.QuickCourse;
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -11,6 +12,6 @@ import java.util.Map;
 public class QuickSearchData {
 
     private Map<String, Object> tags = new HashMap<>();
-    private List<QuickCourse> course;
+    private List<QuickCourse> course = new ArrayList<>();
 
 }
